7666f14a401b16e747e905322e81bb901462e34d
Manpreet Kaur <40593717+Manpreet55@users.noreply.github.com>
2019-04-16 23:50:58 -0400

    Add files via upload

---
+++ project/portfolio.js
@@ -0,0 +1,135 @@
+export const portfolioJson = [
+  {
+    title: "Augmented Reality",
+    purpose:
+      "To generate web-based animated content using A-Frame, an augmented reality library which uses paper-based markers",
+    "//":
+      "THE TECH ARRAY BELOW CAN HAVE ANY NUMBER OF ELEMENTS. JUST ENSURE A URL IS ENTERED FOR THE VALUES",
+    tech: [
+      { tool1: "https://google.ca" },
+      { tool2: "https://stackoverflow.com" }
+    ],
+    desc: "",
+    url: "https://codepen.io/manpreet2019/pen/gyGRNL"
+  },
+  {
+    title: "splitText / Draggable",
+    purpose:
+      "To generate web-based animated content and incorporate keyframes and animation paths using Webflow",
+    tech: [{ tool1: "https://google.ca" }],
+    desc: "",
+    url: " https://codepen.io/manpreet2019/pen/WWEPVx"
+  },
+  {
+    title: "Adobe Animate or Webflow",
+    purpose:
+      "To generate web-based animated content and incorporate keyframes and animation paths using Adobe Animate",
+    tech: [{ tool1: "https://google.ca" }],
+    desc: "",
+    url: ""
+  },
+  {
+    title: "animation paths",
+    purpose:
+      "To learn how to incorporate animation along a path using Greensock",
+    tech: [{ tool1: "https://google.ca" }],
+    desc: "",
+    url: "https://codepen.io/manpreet2019/pen/rbpoQo"
+  },
+  {
+    title: "drawSVG",
+    purpose:
+      "To learn how to use Greensock's plugins: drawSVG, splitText, Draggable",
+    tech: [{ tool1: "https://google.ca" }],
+    desc: " ",
+    url: " https://codepen.io/manpreet2019/pen/eoExzz"
+  },
+  {
+    title: "Banner Ad",
+    purpose:
+      "To incorporate timelines and employ scripting to control and animate sprite images",
+    tech: [{ tool1: "https://google.ca" }],
+    desc: "",
+    url: "./banner/banner_ad.html"
+  },
+  {
+    title: "morphSVG",
+    purpose:
+      "To learn how to use Greensock's plugin called morphSVG to morph between two SVG images",
+    tech: [{ tool1: "https://google.ca" }],
+    desc: "",
+    url: ""
+  },
+  {
+    title: "timelines",
+    purpose:
+      "To incorporate timelines and employ scripting to control and animate svg elements",
+    tech: [{ tool1: "https://google.ca" }],
+    desc: "",
+    url: "https://codepen.io/Manpreet222/pen/GemKBz"
+  },
+  {
+    title: " SVG Bending Line ",
+    purpose:
+      " To understand SVG path data and how TweenMax can animate between 2 different sets of them ",
+    tech: [
+      { google: "https://google.ca" },
+      { georgian: "https://georgiancollege.ca" },
+      { codepen: "https://codepen.io" },
+      { google: "https://google.ca" },
+      { georgian: "https://georgiancollege.ca" },
+      { google: "https://google.ca" }
+    ],
+    desc:
+      "Using the SVG Path builder, I created 2 svg paths and used the generated path coordinates which TweenMax used to animate. The result was a bending line similar to a wagging tail. ",
+    url: " https://codepen.io/Manpreet222/pen/GemKBz "
+  },
+  {
+    title: "Toggle",
+    purpose:
+      "To construct a popular user-interface such as the toggle that incorporates animation and to maniuplate elements based on user-intitiated events",
+    tech: [{ tool1: "https://google.ca" }],
+    desc: "",
+    url: ""
+  },
+  {
+    title: "jQuery animation",
+    purpose:
+      "To learn and use jQuery's slide and fade effects, jQuery UI's animations, and Penner's easing plugin",
+    tech: [{ tool1: "https://google.ca" }],
+    desc: "",
+    url: ""
+  },
+  {
+    title: "@keyframes",
+    purpose:
+      "To incorporate pure CSS keyframe animations along with transforms and transitions",
+    tech: [{ tool1: "https://google.ca" }],
+    desc: "",
+    url: "./Lab5/Lab5/index.html"
+  },
+  {
+    title: "Sprite animation",
+    purpose:
+      "To emulate Twitter's Like button via sprite-based animation using keyframes and steps",
+    tech: [{ tool1: "https://google.ca" }],
+    desc: "",
+    url: ""
+  },
+  {
+    title: "Storyboard",
+    purpose:
+      "To develop a simple storyboard to describe an animated sequence or a user interaction.",
+    tech: [{ tool1: "https://google.ca" }],
+    desc: "The description of this is that the site is based upon the beauty of the women  for which the women can purchase beauty products  ",
+    url: "./assets/storyboard.pdf"
+  },
+  {
+    title: "The 12 principles",
+    purpose:
+      "To explain the basic elements and principles of animation/motion graphics in support of web-based interface design",
+    tech: [{ tool1: "https://google.ca" }],
+    desc: "",
+    url: ""
+  }
+];
